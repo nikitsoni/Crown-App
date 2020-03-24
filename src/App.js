c4798615c68a533bc673a139c034fabcc7d3b2dc
nikitsoni <47641941+nikitsoni@users.noreply.github.com>
2020-03-24 19:49:54 +0530

    made header component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import React from 'react';
 import './App.css';
 import {Switch, Route} from 'react-router-dom';
 import HomePage from '../src/Pages/homepage/homepage.component';
+import ShopPage from '../src/Pages/Shop/shoppage.component';
+import Header from './Component/Header/header.component'
 
-const HatsPage = () => (
-  <div>
-    <h1>HatsPage</h1>
-  </div>
-)
 
 
 function App() {
   return (
     <div className="App">
+    <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />>
+        <Route path='/shop' component={ShopPage} />>
       </Switch>
     </div>
   );
---
+++ src/Pages/Shop/shoppage.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import SHOP_DATA from './shop-data';
+import CollectionPreview from '../../Component/Collection-preview/collection-preview.component'
+
+class ShopPage extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            collections : SHOP_DATA
+        };
+    }
+
+
+    render(){
+        const {collections} = this.state;
+        return(
+            <div>
+                {
+                    collections.map(({id, ...otherSectionCollection}) => (
+                        <CollectionPreview key={id} {...otherSectionCollection} />
+                    ))
+                }
+            </div>
+        )
+    }
+
+}
+
+export default ShopPage;
